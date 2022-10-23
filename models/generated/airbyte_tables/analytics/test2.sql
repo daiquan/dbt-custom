1441c4a3ee0673cd88818739772517bbbb97532e
qdai <qdai@korewireless.com>
2022-10-23 09:19:42 -0500

    do not use ref

--- models/generated/airbyte_tables/analytics/test2.sql
+++ models/generated/airbyte_tables/analytics/test2.sql
@@ -3,11 +3,10 @@
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
--- depends_on: {{ ref('_airbyte_raw_test1') }}
 select
     org_urn as orgUrn,
     time_stamp,
     subscription_urn as subUrn
-from {{ ref('_airbyte_raw_test1') }}
+from _airbyte_raw_test1
 where 1 = 1
 
