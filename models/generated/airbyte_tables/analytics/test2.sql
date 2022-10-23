37d516859c847b07e075296d05e5e1b0072fa99b
qdai <qdai@korewireless.com>
2022-10-23 09:09:07 -0500

    chagne depends on _airbyte_raw_test1

--- models/generated/airbyte_tables/analytics/test2.sql
+++ models/generated/airbyte_tables/analytics/test2.sql
@@ -3,11 +3,11 @@
     tags = [ "top-level" ]
 ) }}
 -- Final base SQL model
--- depends_on: {{ ref('test1') }}
+-- depends_on: {{ ref('_airbyte_raw_test1') }}
 select
     org_urn as orgUrn,
     time_stamp,
     subscription_urn as subUrn
-from {{ ref('test1') }}
+from {{ ref('_airbyte_raw_test1') }}
 where 1 = 1
 
